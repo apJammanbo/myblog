dce63721ad3911186aeb335ed9ed914eaed7779e
apJammanbo <apJammanbo@gmail.com>
2018-04-07 15:57:26 +0900

    2차 init

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,16 +3,16 @@ import Helmet from 'react-helmet';
 import '../assets/scss/init.scss';
 
 class Layout extends React.Component {
-  render() {
-    const { children } = this.props;
+	render() {
+		const { children } = this.props;
 
-    return (
-      <div className="layout">
-        <Helmet defaultTitle="Blog by John Doe" />
-        {children()}
-      </div>
-    );
-  }
+		return (
+            <div className="layout">
+              <Helmet defaultTitle="Jammanbo's Blog" />
+				{children()}
+            </div>
+		);
+	}
 }
 
 export default Layout;
