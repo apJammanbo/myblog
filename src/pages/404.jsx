c9e172469fbb4a207866ffdd8cfe77ef54b4ebd3
apJammanbo <apJammanbo@gmail.com>
2018-04-07 15:49:35 +0900

    Revert "init"
    This reverts commit 4bab26c4c38c15aedded769d7b87cd2565ebe081.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,47 +2,48 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 
 class NotFoundRoute extends React.Component {
-	render() {
-		return (
-			<div>
-				<Sidebar {...this.props} />
-				<div className="content">
-					<div className="content__inner">
-						<div className="page">
-							<h1 className="page__title">NOT FOUND</h1>
-							<div className="page__body">
-								<img style={{ width: '100%' }} src="404.png" alt={'페이지가 없습니다.'} />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Sidebar {...this.props} />
+        <div className="content">
+          <div className="content__inner">
+            <div className="page">
+              <h1 className="page__title">NOT FOUND</h1>
+              <div className="page__body">
+                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotFoundRoute;
 
 export const pageQuery = graphql`
-  	query NotFoundQuery {
-    	site {
-      		siteMetadata {
-        		title
-        		subtitle
-        		copyright
-        		menu {
-          			label
-          			path
-        		}
-        		author {
-          			name
-          			email
-          			telegram
-          			twitter
-          			github
-          			rss
-          			vk
-        		}
-      		}
-    	}
-  	}`;
+  query NotFoundQuery {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        menu {
+          label
+          path
+        }
+        author {
+          name
+          email
+          telegram
+          twitter
+          github
+          rss
+          vk
+        }
+      }
+    }
+  }
+`;
